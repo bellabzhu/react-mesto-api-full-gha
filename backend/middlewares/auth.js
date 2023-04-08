f78f88acb80fe3d4e49fce4f8b58976bfa45cd26
Bella Bzhukova <bellabzhu@gmail.com>
2023-04-08 18:02:16 +0000

    check if token exists in auth middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,6 +5,9 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = async (req, res, next) => {
   const { token } = req.cookies;
+  if (!token) { 
+    next(new Error401('Необходима авторизация.'));
+  };
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'you-shall-not-pass-hooman');
